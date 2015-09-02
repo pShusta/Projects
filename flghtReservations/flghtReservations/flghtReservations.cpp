9cfbbe7fe98e9c3c3e409a2446f2b84f2349ee63
Phillip Shusta <pshusta@animusinteractive.com>
2015-09-02 11:47:05 -0500

    Same as last push
    ^

---
+++ flghtReservations/flghtReservations/flghtReservations.cpp
@@ -0,0 +1,26 @@
+// flghtReservations.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void readFile(istream, string[]);
+void writeFile(ostream, string[])
+void reserveSeat(string[]);
+void printMap();
+
+
+class Plane{
+public:
+	void printMap();
+private:
+};
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
